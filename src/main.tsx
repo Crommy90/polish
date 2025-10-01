1c2c58e0ef2bf85dce9d5275f7ed039124e1a55b
joshua_crompton <josh.m.crompton@gmail.com>
2025-10-01 20:12:09 +0100

    Try again

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen' // Generated file
 import './index.css' // Import CSS later
 
-const router = createRouter({ routeTree })
+const hashHistory = createHashHistory()
+
+const router = createRouter({ routeTree, history: hashHistory  })
 
 // Register router for type-safety
 declare module '@tanstack/react-router' {
