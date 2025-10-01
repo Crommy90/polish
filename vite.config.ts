b52081c931ba5103743e1ddae489bb844b8e3f90
joshua_crompton <josh.m.crompton@gmail.com>
2025-10-01 20:09:02 +0100

    Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite' // <--- Add this
 import tailwindcss from '@tailwindcss/vite'
 
-
+// Get the base path from environment variables (defaults to '/' for local dev)
+const BASE_URL = process.env.VITE_APP_BASE_URL || '/';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
@@ -21,6 +22,6 @@ export default defineConfig(({ command }) => {
   // Add a temporary base for GitHub Pages, we'll refine this later
   // Replace 'YOUR_REPO_NAME' with the actual name of your GitHub repository
   // Set base to the repository name ONLY if we are building for production
-  base: command === 'build' ? `/polish/` : '/',
+  base: command === 'build' ? BASE_URL : '/',
 }
 })
