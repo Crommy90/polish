0a63308a8752016f5b1a690b8ccf9751e0029752
joshua_crompton <josh.m.crompton@gmail.com>
2025-10-06 17:57:53 +0100

    Try fix github page routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import path from "path"
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite' // <--- Add this
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite'; // <--- Add this
+import react from '@vitejs/plugin-react';
+import path from "path";
+import { defineConfig } from 'vite';
 
-// Get the base path from environment variables (defaults to '/' for local dev)
-const BASE_URL = process.env.VITE_APP_BASE_URL || '/';
+
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
   plugins: [react(),
   tanstackRouter(),
@@ -22,6 +21,6 @@ export default defineConfig(({ command }) => {
   // Add a temporary base for GitHub Pages, we'll refine this later
   // Replace 'YOUR_REPO_NAME' with the actual name of your GitHub repository
   // Set base to the repository name ONLY if we are building for production
-  base: command === 'build' ? BASE_URL : '/',
+  base: '/polish/',
 }
 })
