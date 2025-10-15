52df1fccdf0d549e7102f944bcf1989b8e5c4ae9
joshua_crompton <josh.m.crompton@gmail.com>
2025-10-15 23:50:30 +0100

    More verbs, find and deleted duplicates

--- src/components/verbs/verbs-common.tsx
+++ src/components/verbs/verbs-common.tsx
@@ -1,10 +1,10 @@
 // Import the JSON data
-import { Flex, Grid, type SectionProps } from "@radix-ui/themes";
-import verbs from "../../data/verbs.json";
+import { Flex, Grid, type SectionProps } from '@radix-ui/themes';
+import verbs from '../../data/verbs.json';
 
-
-import { Section } from "../app-ui/section";
-import { Table } from "../app-ui/table";
+import { Section } from '../app-ui/section';
+import { SectionTitle } from '../app-ui/section-title';
+import { Table } from '../app-ui/table';
 
 // Define the type for the verb structure
 interface Verb {
@@ -16,18 +16,59 @@ interface Verb {
   };
 }
 
+function findDuplicateVerbs(verbs: Verb[]): Verb[] {
+  const seenEn = new Map<string, Verb>();
+  const seenImp = new Map<string, Verb>();
+
+  const duplicates = new Set<Verb>();
+
+  for (const verb of verbs) {
+    const { en, imp } = verb;
+
+    if (seenEn.has(en)) {
+      duplicates.add(verb);
+      duplicates.add(seenEn.get(en)!);
+    } else {
+      seenEn.set(en, verb);
+    }
+
+    if (imp.length > 1) {
+      if (seenImp.has(imp)) {
+        duplicates.add(verb);
+        duplicates.add(seenImp.get(imp)!);
+      } else {
+        seenImp.set(imp, verb);
+      }
+    }
+  }
+
+  return Array.from(duplicates);
+}
 interface VerbsCommonProps extends SectionProps {
-  level?: number
+  level?: number;
 }
 
 export function VerbsCommon(props: VerbsCommonProps) {
   // Type the imported data
   const verbList: Verb[] = verbs;
   verbList.sort((a, b) => a.en.localeCompare(b.en));
-  const {className, level, ...rest} = {...props}
+  const { className, level, ...rest } = { ...props };
+
+  const duplicates = findDuplicateVerbs(verbList);
 
   return (
-    <Section level={level} className={`w-max max-w-full${className ? ` ${className}` : ""}`} {...rest}>
+    <Section
+      level={level}
+      className={`w-max max-w-full${className ? ` ${className}` : ''}`}
+      {...rest}
+    >
+      <SectionTitle level={level}>Common Verbs</SectionTitle>
+      {duplicates.length > 0 &&
+        duplicates.map((x) => (
+          <span>
+            {x.en}/{x.imp}/{x.per}
+          </span>
+        ))}
       <Table
         headers={['English', 'Polish', 'Forms']}
         rows={verbList.map((d) => [
